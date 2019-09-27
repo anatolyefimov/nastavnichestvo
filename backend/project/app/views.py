dc98f457988d9a381b0fa7c48ecab8c411781929
anatolyefimov <efimo.tolik@gmail.com>
2019-09-27 23:28:08 +0300

    register view

--- backend/project/app/views.py
+++ backend/project/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+import json
+
+def register(request): 
+    req = json.loads(request.body)
+    res = {
+        'status': 'ok'
+    }
+
+    user = User.objects.get(username=req.username)
+    if user is not None:
+        res['status'] = 'already exists'
+    res = json.dumps(res)
+
+     return HttpResponse(res, content_type='application/json')
