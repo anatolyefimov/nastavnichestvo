a2a7f68d1b0f3244fe48911256a016e1504951fd
Avdey <avdeev.1van@icloud.com>
2019-09-28 01:19:46 +0300

    сделал формы

---
+++ frontend/src/components/pages/Registry/index.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import './Registry.css'
+
+class Login extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            surname: '',
+            lastname: '',
+            address: '',
+            login: '',
+            password: '',
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmit() {
+        console.log(this.state.login, this.state.password);
+        this.setState({
+                name: '',
+                surname: '',
+                lastname: '',
+                address: '',
+                login: '',
+                password: '',
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div
+                className="form-container form-container__margin form-container__size-big form-container__border-style">
+                <h2>Присоединяйся</h2>
+                <form className="login-form">
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-m" required
+                           placeholder="Имя" type="text" value={this.state.login} name="name"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-m" required
+                           placeholder="Фамилия" type="password" value={this.state.password} name="surname"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-m" required
+                           placeholder="Отчество" type="text" value={this.state.login} name="lastname"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-big" required
+                           placeholder="Адрес проживания" type="text" value={this.state.password} name="address"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-m" required
+                           placeholder="Login" type="text" value={this.state.login} name="login"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-m" required
+                           placeholder="Password" type="password" value={this.state.password} name="password"/>
+                </form>
+                <button
+                    onClick={this.onSubmit}
+                    className="login-form_button login-form_button__size-m login-form_button__green-theme button">
+                    Присоединиться
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Login;
