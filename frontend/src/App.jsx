cb7ac6ff5896dcde4b2bc2e8a5665dcbd82f1bc1
Avdey <avdeev.1van@icloud.com>
2019-09-28 00:59:51 +0300

    добавляю форму входа и регистрации

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import Header from 'components/Header'
+import Login from 'components/pages/Login'
 
 class App extends React.Component {
     render() {
-        return (<Header/>);
+        return (
+            <div>
+                <Header/>
+                <Route path="/login" component={Login} />
+            </div>
+            );
     }
 }
 
---
+++ frontend/src/components/pages/Login/index.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import './Login.css'
+
+class Login extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            login: '',
+            password: '',
+        };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    onSubmit() {
+        console.log(this.state.login, this.state.password);
+        this.setState(
+            {login: '', password: '',}
+            );
+    }
+
+    render() {
+        return (
+            <div
+                className="form-container form-container__margin form-container__size-small form-container__border-style">
+                <h2>Вход</h2>
+                <form className="login-form">
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-big" required
+                           placeholder="Login" type="text" value={this.state.login} name="login"/>
+                    <input onChange={this.onInputChange} className="login-form_input login-form_input__border login-form_input__size-big" required
+                           placeholder="Password" type="password" value={this.state.password} name="password"/>
+                    <button
+                        onClick={this.onSubmit}
+                        className="login-form_button login-form_button__size-m login-form_button__green-theme button">
+                        Вход
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
