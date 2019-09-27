53ab047cd8f16b643356df076c9d5a6cd63901ce
anatolyefimov <efimo.tolik@gmail.com>
2019-09-27 22:00:50 +0300

    logo fix

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import './Header.css'
+import logo from './logo.png'
 
 function Header() {
     return (
         <div className="Header">
             <a className="Header_link" href="http://www.sgdeti.ru/" target="_blank">
                 <img className="Header_link__img img"
-                     src="logo.png" alt=""/>
+                     src={logo} alt=""/>
                 <h3 className="Header_link__title">Солнечный город</h3>
             </a>
         </div>
