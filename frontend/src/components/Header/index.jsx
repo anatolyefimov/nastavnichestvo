89b08ab0791b3609108b0b293cfffd66e5cb0521
Avdey <avdeev.1van@icloud.com>
2019-09-27 23:27:05 +0300

    сделал хедер

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+
 
 import './Header.css'
 import logo from './logo.png'
 
+import routes from 'routes'
+
 function Header() {
+    const links = routes.map(r => <NavLink key={r.id} className="Header_navlinks" to={r.path}>{r.text}</NavLink>);
+
     return (
         <div className="Header">
             <a className="Header_link" href="http://www.sgdeti.ru/" target="_blank">
-                <img className="Header_link__img img"
+                <img className="Header_img img"
                      src={logo} alt=""/>
-                <h3 className="Header_link__title">Солнечный город</h3>
+                <div className="Header_title-container">
+                    <h3 className="Header_title">Солнечный город</h3>
+                    <h3 className="Header_title-about">Детский благотворительный фонд</h3>
+                </div>
             </a>
+            <div className="Header_links-container">
+                {links}
+            </div>
         </div>
     );
 }
